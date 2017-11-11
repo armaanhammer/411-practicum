b4b0a5c62b607fa6ba19c2b4e0d7235623bd381b
davidyakovlev <Yakovlev.david@gmail.com>
2017-11-11 15:32:29 -0800

    Code
    Code to get the UV and RTC transmitting data

---
+++ Software Scratch/RTC/RTC.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include "RTClib.h"
+RTC_DS1307 RTC;
+
+#define D1 5 // I2C Bus SCL (clock)
+#define D2 4 // I2C Bus SDA (data)
+
+void setup () {
+    Serial.begin(115200);
+    Wire.begin(4,5);
+    RTC.begin();
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+}
+void loop () {
+    DateTime now = RTC.now(); 
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println(); 
+    delay(1000);
+}
